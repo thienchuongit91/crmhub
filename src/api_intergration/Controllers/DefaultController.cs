76af65344b95804f10cfc8426ee462b4f5634e0b
thienchuongit91 <thienchuong.it91@gmail.com>
2019-09-24 22:40:00 +0700

    create check lead api

---
+++ src/api_intergration/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using api_intergration.Handlers;
+
+namespace api_intergration.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpPost]
+        [Route("api/shb/checklead")]
+        public async System.Threading.Tasks.Task<string> CheckLeadAsync([FromBody] string jsonObj)
+        {
+            SHBFCHandlers handlers = new SHBFCHandlers();
+            var result = await handlers.CheckLeadAsync(jsonObj);
+            return result;
+        }
+    }
+}
---
+++ src/api_intergration/Handlers/SHBFCHandlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace api_intergration.Handlers
+{
+    public class SHBFCHandlers
+    {
+        public static string api_base_url = Common.Constant.SHBC_API_BASE();
+        static HttpClient client = new HttpClient();
+        public async Task<string> CheckLeadAsync(string jsonString)
+        {
+            string CheckleadApi = "/crm/ts/v1/checklead";
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(api_base_url + CheckleadApi, content);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            else
+            {
+                return "Error";
+            }
+            
+        }
+    }
+}
