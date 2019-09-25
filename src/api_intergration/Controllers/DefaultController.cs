6315b453a5f129f8274a18fca33a4bba07074872
thienchuongit91 <thienchuong.it91@gmail.com>
2019-09-26 02:17:28 +0700

    adding collection api with the configuration

--- src/api_intergration/Controllers/DefaultController.cs
+++ src/api_intergration/Controllers/DefaultController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web.Http;
 using api_intergration.Handlers;
+using Newtonsoft.Json;
+using api_intergration.Models;
 
 namespace api_intergration.Controllers
 {
@@ -12,11 +16,64 @@ namespace api_intergration.Controllers
     {
         [HttpPost]
         [Route("api/shb/checklead")]
-        public async System.Threading.Tasks.Task<string> CheckLeadAsync([FromBody] string jsonObj)
+        public async Task<string> CheckLeadAsync([FromBody] string jsonObj)
         {
             SHBFCHandlers handlers = new SHBFCHandlers();
             var result = await handlers.CheckLeadAsync(jsonObj);
             return result;
+        }
+
+        [HttpPost]
+        [Route("api/datahub/collect")]
+        public HttpResponseMessage DataCollection([FromBody] dynamic jsonStringObj)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            DataCollectionDbContext dbContext = new DataCollectionDbContext();
+            DataCollection dataCollection = new DataCollection();
+            try
+            {
+                var headers = Request.Headers;
+                if (headers.Contains("client_id") && headers.Contains("client_secret"))
+                {
+                    if (headers.GetValues("client_id").FirstOrDefault() == "eac5d782-3a48-4a23-ae97-002681dc4dfd" && headers.GetValues("client_secret").FirstOrDefault() == "182548a4d4b34e9aaee83380730f4152")
+                    {
+                        string jsonString = JsonConvert.SerializeObject(jsonStringObj);
+                        dataCollection.data = jsonString;
+                        dbContext.dataCollections.Add(dataCollection);
+                        dbContext.SaveChanges();
+                        int dataid = dataCollection.id;
+                        response = Request.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(new String[] {
+                            "Sending data successfully",
+                            "Data id: "+dataid.ToString()+""
+                        }));
+                    }
+                    else
+                    {
+                        response = Request.CreateResponse(HttpStatusCode.Unauthorized, "error: client_id or client_secret is incorrect");
+                    }
+                    
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.BadRequest, JsonConvert.SerializeObject("Missing client_id or client_secret"));
+                    
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+                return response;
+            }
+           
+        }
+        [HttpGet]
+        [Route("api/datahub/test")]
+        public HttpResponseMessage test()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            response = Request.CreateResponse(HttpStatusCode.OK, "TESTING OK!");
+            return response;
         }
     }
 }
---
+++ src/api_intergration/Models/DataCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace api_intergration.Models
+{
+    [Table("DataCollection")]
+    public class DataCollection
+    {
+        [Column("id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Column("data")]
+        [Required]
+        public string data { get; set; }
+
+    }
+    public class DataCollectionDbContext : DbContext
+    {
+        public DataCollectionDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<DataCollection> dataCollections { get; set; }
+    }
+}
