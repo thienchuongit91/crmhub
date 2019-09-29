c69b0ea66cbc8d1a9e1ab3d81716ebd877395729
thienchuongit91 <thienchuong.it91@gmail.com>
2019-09-29 17:19:57 +0700

    adding signalR source code

--- src/crm_data_hub/Global.asax.cs
+++ src/crm_data_hub/Global.asax.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +11,18 @@ namespace crm_data_hub
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        string connString = ConfigurationManager.ConnectionStrings
+        ["DefaultConnection"].ConnectionString;
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            SqlDependency.Start(connString);
+        }
+        protected void Application_End()
+        {
+            //Stop SQL dependency
+            SqlDependency.Stop(connString);
         }
     }
 }
---
+++ src/crm_data_hub/Hubs/DbChangedHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace crm_data_hub.Hubs
+{
+    public class DbChangedHub : Hub
+    {
+        private static string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        [HubMethodName("DBChangedTrigger")]
+        public static void DbChangeTrigger()
+        {
+
+        }
+    }
+}
